ab8a34d32b6529d595b29df95cf9f29a18bf9ea8
Nitesh Sharma <nitesh.sharma@blueyonder.com>
2020-09-29 15:44:53 +0530

    Added new solution

---
+++ App.PromotionEngine/App.PromotionEngine.BusinessLogic.Test/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("App.PromotionEngine.BusinessLogic.Test")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("JDA Software Inc.")]
+[assembly: AssemblyProduct("App.PromotionEngine.BusinessLogic.Test")]
+[assembly: AssemblyCopyright("Copyright © JDA Software Inc. 2020")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("a60b497f-d509-4288-b6bd-15172009994e")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
